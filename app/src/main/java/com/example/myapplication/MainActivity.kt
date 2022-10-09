c77f2f4c60883aed0d61305013b65bd67ce5e932
Undervis <Undervis@gmail.com>
2022-10-10 00:17:03 +0500

    9

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,47 +1,68 @@
 package com.example.myapplication
 
+import android.annotation.SuppressLint
 import android.content.Context
-import android.content.Intent
-import android.graphics.Typeface
+import android.graphics.Color
 import android.net.ConnectivityManager
 import android.net.NetworkInfo
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.res.ResourcesCompat
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import com.shuhart.materialcalendarview.CalendarDay
+import com.shuhart.materialcalendarview.MaterialCalendarView
 import com.squareup.picasso.Picasso
+import com.shuhart.materialcalendarview.OnDateSelectedListener
+import com.shuhart.materialcalendarview.indicator.MonthIndicator
+import com.shuhart.materialcalendarview.indicator.pager.PagerIndicator
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnDateSelectedListener {
+
+    private var tvTitle: TextView? = null
+    private var tvDescription: TextView? = null
+    private var tvDate: TextView? = null
+    private var imPhoto: ImageView? = null
+
+    val dates = ArrayList<String>()
+    val tittle = ArrayList<String>()
+    val descriptions = ArrayList<String>()
+    val imgs = ArrayList<String>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var cal: CalendarView = findViewById(R.id.y)
-        // var scroll: LinearLayout = findViewById(R.id.scrooler)
+        val cal: MaterialCalendarView = findViewById(R.id.calendarView)
+        tvTitle = findViewById(R.id.tvTitle)
+        tvDescription = findViewById(R.id.tvDescription)
+        tvDate = findViewById(R.id.tvDate)
+        imPhoto = findViewById(R.id.imPhoto)
+        imPhoto!!.clipToOutline = true
 
-        var database = Firebase.database
-        var myRef = database.getReference("date")
-        var dates = ArrayList<String>()
-        var tittle = ArrayList<String>()
-        var desriptions = ArrayList<String>()
-        var imgs = ArrayList<String>()
+        tvTitle!!.visibility = View.INVISIBLE
+        tvDate!!.visibility = View.INVISIBLE
+        tvDescription!!.visibility = View.INVISIBLE
+        imPhoto!!.visibility = View.INVISIBLE
+
+        val database = Firebase.database
+        val myRef = database.getReference("date")
 
         myRef.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
 //                if (dates.size>0)
 //                    dates.clear()
-                for (i in dataSnapshot.children)
-                {
-                    var fbe = i.getValue(fb().javaClass)
+                for (i in dataSnapshot.children) {
+                    val fbe = i.getValue(fb().javaClass)
                     dates.add(fbe?.getDates().toString())
                     tittle.add(fbe?.getTitles().toString())
-                    desriptions.add(fbe?.getDescription().toString())
+                    descriptions.add(fbe?.getDescription().toString())
                     imgs.add(fbe?.getImage().toString())
                 }
             }
@@ -49,41 +70,47 @@ class MainActivity : AppCompatActivity() {
             override fun onCancelled(databaseError: DatabaseError) {}
         })
 
+        Log.i("abobus", cal.monthIndicator.toString())
 
-        cal.setOnDateChangeListener { view, year, month, dayOfMonth ->
-            var m = month + 1
-            var key = "$dayOfMonth.$m"
-            var scrool: LinearLayout = findViewById(R.id.scrooler)
-            scrool.removeAllViews()
+        cal.selectionMode = MaterialCalendarView.SELECTION_MODE_SINGLE
+        cal.addOnDateChangedListener(this)
+    }
 
-            if ( !isOnline(this) ){
-                Toast.makeText(this, "Подключитесь к интернету", Toast.LENGTH_SHORT).show();
-                return@setOnDateChangeListener
-            }
+    override fun onDateSelected(widget: MaterialCalendarView, date: CalendarDay, selected: Boolean) {
 
-            try {
-            getDates("$dayOfMonth.$m", dates,tittle, desriptions, imgs, scrool)}
-            catch (E: IndexOutOfBoundsException)
-            {
-                Toast.makeText(this, "Ошибка: $E", Toast.LENGTH_SHORT)
-                    .show()
-                return@setOnDateChangeListener
-            }
+        val m = date.month + 1
 
-            try {
+        tvTitle!!.visibility = View.INVISIBLE
+        tvDate!!.visibility = View.INVISIBLE
+        tvDescription!!.visibility = View.INVISIBLE
+        imPhoto!!.visibility = View.INVISIBLE
+
+        if (!isOnline(this@MainActivity)) {
+            Toast.makeText(this, "Подключитесь к интернету", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        try {
+            getDates("${date.day}.$m", dates, tittle, descriptions, imgs)
+        } catch (E: IndexOutOfBoundsException) {
+            Toast.makeText(this@MainActivity, "Ошибка: $E", Toast.LENGTH_SHORT)
+                .show()
+            return
+        }
+
+        try {
+
             for (i in m..12) {
-                for (k in dayOfMonth..30) {
+                for (k in date.day + 1..31) {
                     if (getAdvice("$k.$m", dates)) {
-                        Toast.makeText(this, "Самое ближнее событие: $k.$m", Toast.LENGTH_SHORT)
+                        Toast.makeText(this@MainActivity, "Ближайшее событие: $k.$m", Toast.LENGTH_SHORT)
                             .show()
-                        return@setOnDateChangeListener
+                        return
                     }
                 }
             }
-            }
-            catch (E: IndexOutOfBoundsException){
-                return@setOnDateChangeListener
-            }
+        } catch (E: IndexOutOfBoundsException) {
+            return
         }
     }
 
@@ -91,8 +118,8 @@ class MainActivity : AppCompatActivity() {
         key: String,
         list: ArrayList<String>
     ): Boolean {
-        for (i in 0..list.size-1) {
-            var open = list[i].dropLastWhile { it != '.' }.dropLast(1)
+        for (i in 0..list.size - 1) {
+            val open = list[i].dropLastWhile { it != '.' }.dropLast(1)
             if (open == key)
                 return true
         }
@@ -104,47 +131,56 @@ class MainActivity : AppCompatActivity() {
         dates: ArrayList<String>,
         title: ArrayList<String>,
         desc: ArrayList<String>,
-        imgs: ArrayList<String>,
-        scrool: LinearLayout
-    ){
-        for (i in 0..dates.size -1) {
-            var open = dates[i].dropLastWhile { it != '.' }.dropLast(1)
+        imgs: ArrayList<String>
+    ) {
+        for (i in 0..dates.size - 1) {
+            val open = dates[i].dropLastWhile { it != '.' }.dropLast(1)
             if (open == key) {
-                var txt: TextView = TextView(this)
-                val face = ResourcesCompat.getFont(this, R.font.activist);
-                txt.setTypeface(face)
-                txt.textSize = 40f
+                val str = sentData(dates[i].dropLastWhile { it != '.' }.dropLast(1)
+                    .dropLastWhile { it != '.' }.dropLast(1).toInt(),
+                    dates[i].dropLastWhile { it != '.' }.dropLast(1).dropWhile { it != '.' }.drop(1)
+                        .toInt(),
+                    dates[i].dropWhile { it != '.' }.drop(1).dropWhile { it != '.' }.drop(1).toInt()
+                )
+                tvTitle!!.visibility = View.VISIBLE
+                tvDate!!.visibility = View.VISIBLE
+                tvDescription!!.visibility = View.VISIBLE
 
+                tvTitle!!.text = title[i].capitalize()
+                tvDescription!!.text = desc[i].capitalize()
+                tvDate!!.text = str
 
-                var str = sentData(dates[i].dropLastWhile { it != '.' }.dropLast(1).dropLastWhile { it != '.' }.dropLast(1).toInt(),
-                    dates[i].dropLastWhile { it != '.' }.dropLast(1).dropWhile { it != '.' }.drop(1).toInt(),
-                    dates[i].dropWhile { it != '.' }.drop(1).dropWhile { it != '.' }.drop(1).toInt())
-                txt.text = "${str}: ${title[i]}-\n${desc[i]}\n\n"
-                scrool.addView(txt)
-                if (imgs[i] != "null")
-                {
-                    var img: ImageView = ImageView(this)
-                    Picasso.with(this).load(imgs[i]).placeholder(R.drawable.img).error(R.drawable.img).into(img)
-                    scrool.addView(img)
+                Log.i("aboba", imgs[i])
+
+                if (imgs[i] != "null") {
+                    imPhoto!!.visibility = View.VISIBLE
+                    try {
+                        Picasso.get().load(imgs[i]).placeholder(R.drawable.img)
+                            .error(R.drawable.img).into(imPhoto)
+                    } catch(E: java.lang.IllegalArgumentException) {
+                        imPhoto!!.visibility = View.INVISIBLE
+                        Toast.makeText(this, "Ошибка загрузки изображения", Toast.LENGTH_SHORT).show()
+                        return
+                    }
+
                 }
             }
         }
     }
 
-    fun sentData(day: Int, mounth: Int, year: Int) : String
-    {
-        var str =""
+    fun sentData(day: Int, mounth: Int, year: Int): String {
+        var str = ""
         if (day < 10)
             str += "0${day.toString()}."
         else
-            str+="$day."
+            str += "$day."
 
-        if(mounth < 10)
+        if (mounth < 10)
             str += "0${mounth.toString()}."
         else
-            str+="$mounth."
+            str += "$mounth."
 
-        str+= year.toString()
+        str += year.toString()
 
         return str
     }
